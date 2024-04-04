132d85d0917cc4f2f1780ab60cdc191f24d8ce58
KArawattigi <kevinarawattigi@icloud.com>
2024-04-04 02:28:48 -0400

    final code

--- customer_banking.py
+++ customer_banking.py
@@ -3,20 +3,27 @@
 from savings_account import create_savings_account
 from cd_account import create_cd_account
 
-#prompt string is printed to standard output  
-#data_type this is the data type for the input that the user is expectedto enter  
-def get_valid_user_input(prompt, data_type):
+
+
+# prompt : string is printed to standard output promptingthe user for an input  
+# data_type : this is the data type for the input that the user is expected to enter 
+# get_user_input frunction converts the userinput to the expected datatype   
+def get_user_input(prompt, data_type):
     while True:
         input_value = input(prompt)
         try:
+            # default input value to 0 if escape sequence is entered  
+            input_value = 0 if input_value=="~~N" else input_value
             # Try to convert the user input to the specified data type
-            # this code may raise an exception
+            # if an exception occurs it means the datatype is invalid 
             input_value = data_type(input_value)
             return input_value
         except ValueError:
-            # If the conversion fails, print an error message and prompt again
-            print("#Error 001 : Invalid input. Please enter a valid ", data_type.__name__)
-    
+            # print the error message and prompt the user again
+            print(f"#Error 001 : Invalid input. Please enter a valid {data_type.__name__} ")
+            print("Enter ~~N to Exit input value will default to 0")
+
+
 
 # Define the main function
 def main():
@@ -26,9 +33,9 @@ def main():
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
-    savings_balance = get_valid_user_input("Enter the initial balance for the Saving Account :",float)
-    savings_interest = get_valid_user_input("Enter the interest rate (apr) for the Saving Account :",float)
-    savings_maturity = get_valid_user_input("Enter the number of months for the Saving Account :",int)
+    savings_balance = get_user_input("Enter the initial balance for the Saving Account :",float)
+    savings_interest = get_user_input("Enter the interest rate (apr) for the Saving Account :",float)
+    savings_maturity = get_user_input("Enter the number of months for the Saving Account :",int)
 
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
@@ -40,9 +47,9 @@ def main():
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
     
-    cd_balance = get_valid_user_input("Enter the initial balance for the CD Account :",float)
-    cd_interest = get_valid_user_input("Enter the interest rate (apr) for the CD Account :",float)
-    cd_maturity = get_valid_user_input("Enter the number of months for the CD Account :",int)
+    cd_balance = get_user_input("Enter the initial balance for the CD Account :",float)
+    cd_interest = get_user_input("Enter the interest rate (apr) for the CD Account :",float)
+    cd_maturity = get_user_input("Enter the number of months for the CD Account :",int)
 
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
